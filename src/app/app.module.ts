82c0149fe47c6f23a61e27668543439ba6266a85
Amir <amir.pincu@gmail.com>
2020-03-24 17:30:23 +0200

    welcome component shifted, modules downloaded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CityWeatherComponentComponent } from './city-weather-component/city-weather-component.component';
 import { CitiesWeatherViewerComponentComponent } from './cities-weather-viewer-component/cities-weather-viewer-component.component';
-import { WelcomeComponent } from './welcome/welcome.component';
 import { WelcomeComponentComponent } from './welcome-component/welcome-component.component';
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: WelcomeComponentComponent },
+  { path: 'weather', component: CitiesWeatherViewerComponentComponent },
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     CityWeatherComponentComponent,
     CitiesWeatherViewerComponentComponent,
-    WelcomeComponent,
     WelcomeComponentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    CommonModule,
+    AppRoutingModule,
+    RouterModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
